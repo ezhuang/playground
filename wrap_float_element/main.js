be24e69387c07d6100f909bbaff1aaaf9f8a464b
ezhuang <ezhuang@purdue.edu>
2017-11-10 12:57:31 -0500

    first commit

---
+++ wrap_float_element/main.js
@@ -0,0 +1,20 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function hightlightBorder(selector, borderWidth, borderStyle) {
+    if (!borderWidth) borderWidth = "1px"
+    if (!borderStyle) borderStyle = "solid"
+    var all = document.querySelectorAll(selector? selector : "body *");
+
+    for (var i=0, max=all.length; i < max; i++) {
+        all[i].style.border = borderWidth + " " + borderStyle + " " + getRandomColor();
+    }
+}
+
+hightlightBorder();
